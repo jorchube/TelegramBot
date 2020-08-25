fda6113201ac1f45f23d9da931524f5068341456
Jordi Chulia <jorchube.dev@gmail.com>
2020-08-25 13:03:27 +0200

    Started definition of TelegramBotInterface

---
+++ TelegramBot/TelegramBotInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public interface TelegramBotInterface
+    {
+        public void Start();
+        public void Stop();
+
+        public List<TelegramBotUpdate> GetUpdates();
+        public void HandleUpdate(TelegramBotUpdate update);
+    }
+}
