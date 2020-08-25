e589063edd8a5fb33c03fd56c4e5ff2feb07767d
Jordi Chulia <jorchube.dev@gmail.com>
2020-08-25 13:00:22 +0200

    Added TelegramBotRunner

---
+++ TelegramBot/TelegramBotRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public class TelegramBotRunner : TelegramBotRunnerInterface
+    {
+        public void Start(TelegramBotInterface bot)
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
---
+++ TelegramBot/TelegramBotRunnerInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public interface TelegramBotRunnerInterface
+    {
+        public void Start(TelegramBotInterface bot);
+        
+        public void Stop();
+    }
+}
