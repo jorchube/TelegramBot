928554e4b9a63ba2b3ff51e2e6570c682b65dca2
Jordi Chulia <jorchube.dev@gmail.com>
2020-08-25 12:59:39 +0200

    extendedTelegramBotApiClientInterface

--- TelegramBot/TelegramBotApiClient.cs
+++ TelegramBot/TelegramBotApiClient.cs
@@ -5,10 +5,20 @@ using System.Text;
 
 namespace TelegramBotApp
 {
-    public class TelegramBotApiClient
+    public class TelegramBotApiClient: TelegramBotApiClientInteface
     {
         public TelegramBotApiClient(string api_token, HttpClient http_client)
         {
+        }
+
+        public List<TelegramBotUpdate> GetUpdates()
+        {
+            return new List<TelegramBotUpdate>();
+        }
+        
+        public void SendMessage(TelegramBotOutgoingMessage message)
+        {
+
         }
     }
 }
---
+++ TelegramBot/TelegramBotApiClientInteface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public interface TelegramBotApiClientInteface
+    {
+        public List<TelegramBotUpdate> GetUpdates();
+        public void SendMessage(TelegramBotOutgoingMessage message);
+    }
+}
