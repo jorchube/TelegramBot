b175f2edded4aed614be1467b1d9efe8eef45deb
Jordi Chulia <jorchube.dev@gmail.com>
2020-08-25 13:04:02 +0200

    Defined TelegramBotOutgoingMessage data class

---
+++ TelegramBot/TelegramBotOutgoingMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public class TelegramBotOutgoingMessage
+    {
+        public TelegramBotOutgoingMessage(long chat_id, string text)
+        {
+            this.chat_id = chat_id;
+            this.text = text;
+        }
+
+        public long chat_id { get; }
+        public string text { get; }
+    }
+}
