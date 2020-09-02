41e850d7c6183417ecd074e22e886ebcf924947c
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-02 12:23:29 +0200

    Added UriStringBuilder class

---
+++ TelegramBot/UriStringBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public class UriStringBuilder
+    {
+        string api_token;
+        string endpoint;
+        Dictionary<string, string> query_parameters;
+
+        public UriStringBuilder(string api_token, string endpoint)
+        {
+            query_parameters = new Dictionary<string, string>();
+
+            this.api_token = api_token;
+            this.endpoint = endpoint;
+        }
+
+        public UriStringBuilder WithQueryParameter(string name, string value)
+        {
+            query_parameters.Add(name, value);
+
+            return this;
+        }
+
+        public string Build()
+        {
+            string uri = "https://api.telegram.org/bot" + api_token + "/" + endpoint;
+
+            uri = AddQueryParameters(uri);
+
+            return uri;
+        }
+
+        string AddQueryParameters(string uri)
+        {
+            string new_uri;
+
+            if (query_parameters.Count == 0) {
+                return uri;
+            }
+
+            new_uri = uri + "?";
+
+            foreach (KeyValuePair<string, string> pair in query_parameters) {
+                new_uri = new_uri + pair.Key + "=" + pair.Value;
+                if (!pair.Equals(query_parameters.Last())) {
+                    new_uri += "&";
+                }
+            }
+
+            return new_uri;
+        }
+    }
+}
---
+++ TelegramBotTests/UriStringBuilderTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TelegramBotApp;
+
+namespace TelegramBotTests
+{
+    class UriStringBuilderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ItCreatesUriString()
+        {
+            UriStringBuilder builder = new UriStringBuilder("token", "an_endpoint");
+
+            Assert.AreEqual("https://api.telegram.org/bottoken/an_endpoint", builder.Build());
+        }
+
+        [Test]
+        public void ItCreatesUriStringWithOneQueryParameter()
+        {
+            UriStringBuilder builder = new UriStringBuilder("token", "an_endpoint");
+            builder.WithQueryParameter("param", "value");
+
+            Assert.AreEqual("https://api.telegram.org/bottoken/an_endpoint?param=value", builder.Build());
+        }
+
+        [Test]
+        public void ItCreatesUriStringWithManyQueryParameters()
+        {
+            UriStringBuilder builder = new UriStringBuilder("token", "an_endpoint");
+            builder.WithQueryParameter("param1", "value1");
+            builder.WithQueryParameter("param2", "value2");
+            builder.WithQueryParameter("param3", "value3");
+
+            Assert.AreEqual("https://api.telegram.org/bottoken/an_endpoint?param1=value1&param2=value2&param3=value3", builder.Build());
+        }
+    }
+}
