3af220c790d6cc77a4a0486d76528fce493ac99a
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-03 21:41:43 +0200

    Implemented TelegramBotRunner

--- TelegramBot/TelegramBotRunner.cs
+++ TelegramBot/TelegramBotRunner.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TelegramBotApp
 {
     public class TelegramBotRunner : TelegramBotRunnerInterface
     {
+        CancellationTokenSource cancellation_token_source;
+
+        public TelegramBotRunner()
+        {
+            cancellation_token_source = new CancellationTokenSource();
+        }
+
         public void Start(TelegramBotInterface bot)
         {
+            Task.Run(() => {
+                while (true) {
+                    HandleBotUpdates(bot);
+                }
+            },
+            cancellation_token_source.Token);
         }
 
         public void Stop()
         {
+            cancellation_token_source.Cancel();
+        }
+
+        private void HandleBotUpdates(TelegramBotInterface bot)
+        {
+            List<TelegramBotUpdate> updates = bot.GetUpdates();
+
+            foreach (TelegramBotUpdate update in updates) {
+                bot.HandleUpdate(update);
+            }
         }
     }
 }
---
+++ TelegramBotTests/TelegramBotRunnerTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TelegramBotApp;
+
+namespace TelegramBotTests
+{
+    public class TelegramBotRunnerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        
+        }
+
+        [Test]
+        public void ItStartsAndStopsABot()
+        {
+            TelegramBotRunner runner = new TelegramBotRunner();
+            BotStub bot = new BotStub();
+
+            runner.Start(bot);
+
+            Thread.Sleep(millisecondsTimeout: 100);
+            runner.Stop();
+
+            Assert.Greater(bot.get_updates_numcalls, 0);
+            Assert.Greater(bot.handle_update_numcalls, 0);
+        }
+
+        class BotStub : TelegramBotInterface
+        {
+            public int get_updates_numcalls = 0;
+            public int handle_update_numcalls = 0;
+
+            public List<TelegramBotUpdate> GetUpdates()
+            {
+                get_updates_numcalls++;
+
+                Thread.Sleep(millisecondsTimeout: 10);
+
+                return new List<TelegramBotUpdate> { new TelegramBotUpdate() };
+            }
+
+            public void HandleUpdate(TelegramBotUpdate update)
+            {
+                handle_update_numcalls++;
+            }
+
+            public void Start()
+            {
+                
+            }
+
+            public void Stop()
+            {
+                
+            }
+        }
+    }
+}
