e4b36bc8863236e41dcd725d77c80a68d865be38
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-03 21:42:30 +0200

    Extended Acceptance Test

--- TelegramBotTests/TelegramBotAcceptanceTest.cs
+++ TelegramBotTests/TelegramBotAcceptanceTest.cs
@@ -1,53 +1,53 @@
 using NUnit.Framework;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using TelegramBotApp;
 using TelegramBotTests.stubs;
-using TelegramBotTests.helpers;
 
 namespace TelegramBotAcceptanceTests
 {
     public class TelegramBotAcceptanceTests
     {
         string API_TOKEN = "apitoken";
-        HttpClientStub http_client_stub;
+        HttpClientWrapperStub http_client_stub;
 
         [SetUp]
         public void Setup()
         {
-            http_client_stub = new HttpClientStub();
+            http_client_stub = new HttpClientWrapperStub();
         }
 
         [TearDown]
         public void Teardown()
         {
-            http_client_stub.Dispose();
         }
 
         [Test]
         public void BotReceivesASaluteAndAnswersTheUser()
         {
             string hello_message = @"{
-                'ok': true,
-                'result': [
+                ""ok"": true,
+                ""result"": [
                     {
-                        'update_id': 153480413,
-                        'message': {
-                            'message_id': 2,
-                            'from': {
-                                'id': 344365009,
-                                'is_bot': false,
-                                'first_name': 'John',
-                                'last_name': 'Doe',
-                                'language_code': 'en'
+                        ""update_id"": 153480413,
+                        ""message"": {
+                            ""message_id"": 2,
+                            ""from"": {
+                                ""id"": 344365009,
+                                ""is_bot"": false,
+                                ""first_name"": ""John"",
+                                ""last_name"": ""Doe"",
+                                ""language_code"": ""en""
                             },
-                            'chat': {
-                                'id': 344365009,
-                                'first_name': 'John',
-                                'last_name': 'Doe',
-                                'type': 'private'
+                            ""chat"": {
+                                ""id"": 344365009,
+                                ""first_name"": ""John"",
+                                ""last_name"": ""Doe"",
+                                ""type"": ""private""
                             },
-                            'date': 1597997582,
-                            'text': 'Hello'
+                            ""date"": 1597997582,
+                            ""text"": ""Hello""
                         }
                     }
                 ]
@@ -61,9 +61,11 @@ namespace TelegramBotAcceptanceTests
 
             InjectUpdate(hello_message);
 
+            Thread.Sleep(millisecondsTimeout: 200);
             bot.Stop();
 
-            Assert.AreEqual("Hello John", SentMessage());
+            Assert.AreEqual("Hello John", SentMessage().text);
+            Assert.AreEqual(344365009, SentMessage().chat_id);
         }
 
         void InjectUpdate(string get_update_response)
@@ -71,9 +73,11 @@ namespace TelegramBotAcceptanceTests
             http_client_stub.InjectGetResponse(get_update_response);
         }
 
-        string SentMessage()
+        TelegramBotOutgoingMessage SentMessage()
         {
-            return http_client_stub.GetSentMessageText();
+            string content = http_client_stub.PostRequestContent();
+
+            return JsonSerializer.Deserialize<TelegramBotOutgoingMessage>(content);
         }
     }
 }
