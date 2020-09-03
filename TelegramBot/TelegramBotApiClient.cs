92cdab27f349c2f4b5c9ad37630b72bac131fef4
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-03 21:42:18 +0200

    Implemented TelegramBotApiClient

--- TelegramBot/TelegramBotApiClient.cs
+++ TelegramBot/TelegramBotApiClient.cs
@@ -1,24 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using static TelegramBotApp.HttpClientWrapperInterface;
 
 namespace TelegramBotApp
 {
     public class TelegramBotApiClient: TelegramBotApiClientInteface
     {
-        public TelegramBotApiClient(string api_token, HttpClient http_client)
+        const int REQUEST_TIMEOUT_SECONDS = 60;
+        const string GET_UPDATES_ENDPOINT = "getupdates";
+        const string SEND_MESSAGE_ENDPOINT = "sendmessage";
+        long get_updates_offset;
+        string api_token;
+        HttpClientWrapperInterface http_client;
+
+        public TelegramBotApiClient(string api_token, HttpClientWrapperInterface http_client)
         {
+            this.api_token = api_token;
+            this.http_client = http_client;
+            this.get_updates_offset = 0;
         }
 
         public List<TelegramBotUpdate> GetUpdates()
         {
-            return new List<TelegramBotUpdate>();
+            List<TelegramBotUpdate> updates;
+            string uri = GetUpdatesUriString(REQUEST_TIMEOUT_SECONDS, get_updates_offset);
+
+            HttpResponseMessageWrapper response = GetRequest(uri);
+
+            updates = TelegramBotApiGetUpdatesDecoder.Decode(response.Content);
+
+            UpdateGetUpdatesRequestOffset(updates);
+
+            return updates;
         }
-        
+
         public void SendMessage(TelegramBotOutgoingMessage message)
         {
+            string uri = SendMessageUriString();
+            string content = OutgoingMessageEncoder.Encode(message);
 
+            PostRequest(uri, content);
         }
+
+        private string SendMessageUriString()
+        {
+            return new UriStringBuilder(api_token, SEND_MESSAGE_ENDPOINT).Build();
+        }
+
+        private void UpdateGetUpdatesRequestOffset(List<TelegramBotUpdate> updates)
+        {
+            if (updates.Count == 0) {
+                return;
+            }
+
+            List<long> ids = updates.Select(update => update.update_id).ToList();
+
+            get_updates_offset = ids.Max() + 1;
+        }
+
+        private string GetUpdatesUriString(int timeout, long offset)
+        {
+            return new UriStringBuilder(api_token, GET_UPDATES_ENDPOINT).
+                WithQueryParameter("timeout", timeout.ToString()).
+                WithQueryParameter("offset", offset.ToString()).
+                Build();
+        }
+
+        private HttpResponseMessageWrapper GetRequest(string uri)
+        {
+            return http_client.Get(uri);
+        }
+
+        private void PostRequest(string uri, string content)
+        {
+            http_client.Post(uri, content);
+        }
+
     }
 }
---
+++ TelegramBotTests/TelegramBotApiClientTest.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using TelegramBotApp;
+using TelegramBotTests.stubs;
+
+namespace TelegramBotTests
+{
+    class TelegramBotApiClientTest
+    {
+        string TEST_API_TOKEN = "api_token";
+        HttpClientWrapperStub http_client_stub;
+
+        TelegramBotApiClient api_client;
+
+        [SetUp]
+        public void Setup()
+        {
+            http_client_stub = new HttpClientWrapperStub();
+
+            api_client = new TelegramBotApiClient(TEST_API_TOKEN, http_client_stub);
+        }
+
+        [Test]
+        public void IsCreatesANewApiClient()
+        {
+            Assert.IsNotNull(api_client);
+        }
+
+        [Test]
+        public void ItSendsGetUpdatesRequestAndReceivesUpdatesListWithNoUpdates()
+        {
+            List<TelegramBotUpdate> updates;
+            string expected_get_request_uri = "https://api.telegram.org/botapi_token/getupdates?timeout=60&offset=0";
+            http_client_stub.InjectGetResponse(GET_UPDATES_EMPTY_RESPONSE);
+
+            updates = api_client.GetUpdates();
+
+            Assert.IsEmpty(updates);
+            Assert.AreEqual(expected_get_request_uri, http_client_stub.GetRequestUri());
+        }
+
+        [Test]
+        public void ItSendsGetUpdatesRequestAndReceivesUpdatesListWithUpdates()
+        {
+            List<TelegramBotUpdate> updates;
+            string expected_get_request_uri = "https://api.telegram.org/botapi_token/getupdates?timeout=60&offset=0";
+            http_client_stub.InjectGetResponse(GET_UPDATES_ONE_RESPONSE);
+        
+            updates = api_client.GetUpdates();
+
+            Assert.AreEqual(1, updates.Count);
+            Assert.AreEqual(expected_get_request_uri, http_client_stub.GetRequestUri());
+        }
+
+        [Test]
+        public void ItUpdatesGetUpdatesRequestOffsetAccordingToLastUpdatesReceived()
+        {
+            List<TelegramBotUpdate> updates;
+            long expected_offset;
+            string expected_get_request_uri_offset_fmt = "https://api.telegram.org/botapi_token/getupdates?timeout=60&offset={0}";
+            
+            http_client_stub.InjectGetResponse(GET_UPDATES_MANY_UPDATES_RESPONSES);
+
+            updates = api_client.GetUpdates();
+            expected_offset = updates.Last().update_id + 1;
+            _ = api_client.GetUpdates();
+
+            Assert.AreEqual(String.Format(expected_get_request_uri_offset_fmt, expected_offset), http_client_stub.GetRequestUri());
+        }
+
+        [Test]
+        public void ItSendsAMessage()
+        {
+            TelegramBotOutgoingMessage message = new TelegramBotOutgoingMessage(chat_id: 1234, text: "yay");
+            string expected_post_request_uri = "https://api.telegram.org/botapi_token/sendmessage";
+
+            api_client.SendMessage(message);
+
+            Assert.AreEqual(expected_post_request_uri, http_client_stub.PostRequestUri());
+            Assert.AreEqual(@"{""chat_id"":1234,""text"":""yay""}", http_client_stub.PostRequestContent());
+        }
+
+        string GET_UPDATES_EMPTY_RESPONSE = "{\"ok\": true,\"result\": []}";
+        string GET_UPDATES_ONE_RESPONSE = @"{
+            ""ok"": true,
+                ""result"": [
+                {
+                    ""update_id"": 153480413,
+                    ""message"": {
+                        ""message_id"": 2,
+                        ""from"": {
+                            ""id"": 344365009,
+                            ""is_bot"": false,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""language_code"": ""en""
+                        },
+                        ""chat"": {
+                            ""id"": 344365009,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""type"": ""private""
+                        },
+                        ""date"": 1597997582,
+                        ""text"": ""sample text""
+                    }
+                }
+            ]
+        }";
+        string GET_UPDATES_MANY_UPDATES_RESPONSES = @"{
+            ""ok"": true,
+            ""result"": [
+                {
+                    ""update_id"": 153480413,
+                    ""message"": {
+                        ""message_id"": 2,
+                        ""from"": {
+                            ""id"": 344365009,
+                            ""is_bot"": false,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""language_code"": ""en""
+                        },
+                        ""chat"": {
+                            ""id"": 344365009,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""type"": ""private""
+                        },
+                        ""date"": 1597997582,
+                        ""text"": ""sample text""
+                    }
+                },
+                {
+                    ""update_id"": 153480414,
+                    ""message"": {
+                        ""message_id"": 3,
+                        ""from"": {
+                            ""id"": 344365009,
+                            ""is_bot"": false,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""language_code"": ""en""
+                        },
+                        ""chat"": {
+                            ""id"": 344365009,
+                            ""first_name"": ""John"",
+                            ""last_name"": ""Doe"",
+                            ""type"": ""private""
+                        },
+                        ""date"": 1597997586,
+                        ""text"": ""another sample text""
+                    }
+                }
+            ]
+        }";
+    }
+}
