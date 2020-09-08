b0232c1448789a84a8460dad4900060c3fc70df2
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-08 23:26:04 +0200

    Use new Logger in Program.cs

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -7,6 +7,8 @@ namespace TelegramBotApp
 {
     class Program
     {
+        const string APP_NAME = "TelegramBot";
+
         static void Main(string[] args)
         {
             string api_token = args[0];
@@ -15,7 +17,7 @@ namespace TelegramBotApp
             ApiClient api_client = new ApiClient(api_token, http_client);
             BotRunner runner = new BotRunner();
 
-            Bot bot = new Bot(api_client, runner);
+            Bot bot = new Bot(api_client, runner, CreateLogger());
 
             InstallUpdateHandlersToBot(bot);
 
@@ -29,6 +31,11 @@ namespace TelegramBotApp
         static void InstallUpdateHandlersToBot(Bot bot)
         {
             bot.InstallUpdateHandler(new PoliteBotHandler());
+        }
+
+        static LoggerInterface CreateLogger()
+        {
+            return new Logger(APP_NAME, Console.WriteLine, () => DateTime.Now);
         }
     }
 }
