de36c5e963e5949608ba1d14e2e0590823824919
Jordi Chulia <jorchube.dev@gmail.com>
2020-09-08 23:25:10 +0200

    Added LoggerInterface support in Bot

--- TelegramBot/Bot.cs
+++ TelegramBot/Bot.cs
@@ -11,26 +11,34 @@ namespace TelegramBotApp
         BotRunnerInterface runner;
         ApiClientInteface api_client;
         List<UpdateHandlerInterface> update_handlers;
+        LoggerInterface logger;
 
-        public Bot(ApiClientInteface api_client, BotRunnerInterface runner)
+        public Bot(ApiClientInteface api_client, BotRunnerInterface runner, LoggerInterface logger = null)
         {
             this.runner = runner;
             this.api_client = api_client;
             this.update_handlers = new List<UpdateHandlerInterface>();
+            this.logger = logger ?? new NullLogger();
         }
 
         public void Start()
         {
             runner.Start(this);
+
+            this.logger.Log("Started");
         }
 
         public void Stop()
         {
             runner.Stop();
+
+            this.logger.Log("Stopped");
         }
 
         public List<UpdateMessage> GetUpdates()
         {
+            logger.Log("Waiting for updates...");
+
             return api_client.GetUpdates();
         }
 
@@ -41,6 +49,8 @@ namespace TelegramBotApp
 
         public void HandleUpdate(UpdateMessage update)
         {
+            logger.Log($"Handling update from {update.message.from.first_name} {update.message.from.last_name} in {update.message.chat.type} chat -> \"{update.message.text}\"");
+
             foreach (UpdateHandlerInterface handler in update_handlers) {
                 Task.Run(() => handler.HandleUpdate(update, HandleUpdateCallback));
             }
@@ -49,6 +59,15 @@ namespace TelegramBotApp
         public void HandleUpdateCallback(OutgoingMessage response_message)
         {
             api_client.SendMessage(response_message);
+        }
+
+
+        class NullLogger : LoggerInterface
+        {
+            public void Log(string message_format, params object[] args)
+            {
+                return;
+            }
         }
     }
 }
